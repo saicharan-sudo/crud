b080e84a2fe7963826a834ffc31ecd2a5cbc68e9
Sai Charan <vallam-konda.sai-charan@capgemini.com>
2025-04-10 12:53:47 +0530

    initial check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a';
+ngOnInit(): void {
+  this.loadObi();
+}
+  
+  loadObi(){
+    const node = document.createElement('script');
+    node.src = 'https://jsl.prod.obi.aol.com/obipmservice/obick.umd.js';
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
